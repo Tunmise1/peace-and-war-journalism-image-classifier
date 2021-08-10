20c8d39b63b47fa9a0e05f92adf1717000b59446
Tunmise1 <88516285+Tunmise1@users.noreply.github.com>
2021-08-10 23:18:21 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
-from keras.models import load_model
-from keras.utils import np_utils
+import tensorflow as tf
+from tf.keras.models import load_model
+from tf.keras.utils import np_utils
 import cv2, os
 from PIL import Image
 import io
