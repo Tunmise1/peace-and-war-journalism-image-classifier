d5eee32a304d8826f6070519b5ca62e0d72d8de4
Tunmise1 <88516285+Tunmise1@users.noreply.github.com>
2021-08-10 19:59:23 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 import numpy as np
-#from keras.models import load_model
-#from keras.utils import np_utils
+import keras
+from keras.models import load_model
+from keras.utils import np_utils
 #import cv2, os
 from PIL import Image
 import io
